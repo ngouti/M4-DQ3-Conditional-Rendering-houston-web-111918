77ba178c1e85459b1b4e4fe961c19faefe0cb03a
github username <github email address>
2019-01-30 14:30:47 -0600

    done

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -15,19 +15,19 @@ const MenuBar = (props) => {
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a onClick={() => props.setPage(props.profile)} className="item active" id="profile">
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={() => props.setPage(props.photo)} className="item" id="photo">
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={() => props.setPage(props.cocktail)}className="item" id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a onClick={() => props.setPage(props.pokemon)} className="item" id="pokemon"> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -4,21 +4,26 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
 
+  constructor(){
+    super()
+    this.state = {
+      page: Profile
+    }
+  }
 
+  setPage = (pageType) => {
+    this.setState({
+      page: pageType
+    })
+  }
   render() {
-
-    /*
-
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
-
-    */
-
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
+    console.log(Profile)
+   const detailsToDisplay = <div>Hi, I'm a div!</div>
 
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar setPage={this.setPage} profile={Profile} photo={Photos} cocktail={Cocktails} pokemon={Pokemon}/>
+        <this.state.page/>
       </div>
     )
   }
